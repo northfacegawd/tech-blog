a505b4688e2853d9ab9fd7dec43f690f7fb32517
northfacegawd <chuck5732590@gmail.com>
2022-10-04 21:33:33 +0900

    feat: image section animation 추가

--- pages/manifesto.tsx
+++ pages/manifesto.tsx
@@ -7,26 +7,16 @@ import ForthSection from '@components/manifesto/forth-section';
 import FifthSection from '@components/manifesto/fifth-section';
 import SixthSection from '@components/manifesto/sixth-section';
 import SeventhSection from '@components/manifesto/seventh-section';
+import TopImageSection from '@components/common/top-image-section';
 
 const Manifesto: NextPage = function () {
   return (
     <>
-      <div
-        className="h-[70vh] z-0"
-        style={{
-          backgroundImage: 'url(/images/manifesto.webp)',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-        }}
-      >
-        <p className="w-full max-w-3xl mx-auto z-10 pt-[40vh] px-4 md:text-4xl text-shadow-xl shadow-black text-white">
-          Manifesto
-        </p>
-        <p className='"w-full max-w-3xl mx-auto mt-4 z-10 px-4 md:text-2xl text-shadow-xl shadow-black text-white'>
-          제 이력서와 포트폴리오를 추가할 예정입니다.
-        </p>
-      </div>
+      <TopImageSection
+        imageUrl="/images/manifesto.webp"
+        title="Manifesto"
+        description="제 블로그에 담고자 하는 글쓰기 원칙을 소개합니다."
+      />
       <div className="w-full">
         <FirstSection />
         <SecondSection />
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,24 +1,14 @@
+import TopImageSection from '@components/common/top-image-section';
 import { NextPage } from 'next';
 import React from 'react';
 
 const Resume: NextPage = function () {
   return (
-    <div
-      className="h-[70vh] z-0"
-      style={{
-        backgroundImage: 'url(/images/resume.webp)',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100%',
-      }}
-    >
-      <p className="w-full max-w-3xl mx-auto z-10 pt-[40vh] px-4 md:text-4xl text-shadow-xl shadow-black text-white">
-        Resume
-      </p>
-      <p className='"w-full max-w-3xl mx-auto mt-4 z-10 px-4 md:text-2xl text-shadow-xl shadow-black text-white'>
-        제 이력서와 포트폴리오를 추가할 예정입니다.
-      </p>
-    </div>
+    <TopImageSection
+      imageUrl="/images/resume.webp"
+      title="Resume"
+      description="제 이력서와 포트폴리오를 추가할 예정입니다."
+    />
   );
 };
 
