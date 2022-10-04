c83d19dbdf122830d50344b81557e2b0619d415b
northfacegawd <chuck5732590@gmail.com>
2022-10-04 22:45:19 +0900

    fix: 이미지 비율 조정

--- components/common/top-image-section.tsx
+++ components/common/top-image-section.tsx
@@ -12,12 +12,11 @@ const duration = 500;
 const defaultStyle: CSSProperties = {
   opacity: 0,
   transition: `all ${duration}ms ease-out`,
-  backgroundSize: '98%',
 };
 
 const transitionStyle: Partial<Record<TransitionStatus, CSSProperties>> = {
-  entered: { opacity: 1, backgroundSize: 'cover' },
-  entering: { opacity: 1, backgroundSize: '100%' },
+  entered: { opacity: 1 },
+  entering: { opacity: 1 },
   exited: { opacity: 0 },
   exiting: { opacity: 0 },
 };
@@ -37,19 +36,17 @@ export default function TopImageSection({
     <Transition in={render} timeout={duration} mountOnEnter>
       {(state) => (
         <div
-          className="h-[70vh] z-0 overflow-hidden"
+          className="h-[50vh] md:h-[70vh] z-0 overflow-hidden bg-cover bg-center bg-no-repeat"
           style={{
             backgroundImage: `url(${imageUrl})`,
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
             ...defaultStyle,
             ...transitionStyle[state],
           }}
         >
-          <p className="w-full max-w-3xl mx-auto z-10 pt-[40vh] px-4 md:text-4xl text-shadow-xl shadow-black text-white">
+          <p className="w-full max-w-3xl mx-auto z-10  pt-[35vh] md:pt-[40vh] px-4 text-2xl md:text-4xl text-shadow-xl shadow-black text-white">
             {title}
           </p>
-          <p className='"w-full max-w-3xl mx-auto mt-4 z-10 px-4 md:text-2xl text-shadow-xl shadow-black text-white'>
+          <p className='"w-full max-w-3xl mx-auto mt-4 z-10 px-4 text-base md:text-2xl text-shadow-xl shadow-black text-white'>
             {description}
           </p>
         </div>
