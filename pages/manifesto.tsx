e7d3396f54981d12032798b61923b1c8dd814e53
northfacegawd <chuck5732590@gmail.com>
2022-10-04 21:00:58 +0900

    feat: manifesto 상단 이미지 추가

--- pages/manifesto.tsx
+++ pages/manifesto.tsx
@@ -10,15 +10,33 @@ import SeventhSection from '@components/manifesto/seventh-section';
 
 const Manifesto: NextPage = function () {
   return (
-    <div className="pt-14 w-full">
-      <FirstSection />
-      <SecondSection />
-      <ThirdSection />
-      <ForthSection />
-      <FifthSection />
-      <SixthSection />
-      <SeventhSection />
-    </div>
+    <>
+      <div
+        className="h-[70vh] z-0"
+        style={{
+          backgroundImage: 'url(/images/manifesto.webp)',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      >
+        <p className="w-full max-w-3xl mx-auto z-10 pt-[40vh] px-4 md:text-4xl text-shadow-xl shadow-black text-white">
+          Manifesto
+        </p>
+        <p className='"w-full max-w-3xl mx-auto mt-4 z-10 px-4 md:text-2xl text-shadow-xl shadow-black text-white'>
+          제 이력서와 포트폴리오를 추가할 예정입니다.
+        </p>
+      </div>
+      <div className="w-full">
+        <FirstSection />
+        <SecondSection />
+        <ThirdSection />
+        <ForthSection />
+        <FifthSection />
+        <SixthSection />
+        <SeventhSection />
+      </div>
+    </>
   );
 };
 
