4bd3d35519e35f083c5a179ada8a65e8389aed3e
northfacegawd <chuck5732590@gmail.com>
2022-10-04 23:11:23 +0900

    fix: next image로 변경

--- components/common/profile.tsx
+++ components/common/profile.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image';
 import React from 'react';
+
+import cat from '@public/images/min.jpeg';
 
 export default function Profile() {
   return (
     <div className="mx-auto flex max-w-xl flex-col sm:flex-row items-center space-y-4 sm:space-y-0">
       <div className="rounded-full 4 w-24 h-24 overflow-hidden shadow-2xl border-4 border-gray-400 flex-shrink-0">
-        <img src="/images/min.jpeg" alt="profile" className="object-cover" />
+        <Image
+          src={cat}
+          alt="profile"
+          className="object-cover"
+          layout="responsive"
+          width="100"
+          height="100"
+          loading="lazy"
+          placeholder="blur"
+        />
       </div>
       <div className="ml-5 flex flex-col space-y-2 justify-center">
         <p className="text-2xl font-medium">🧑‍💻 이용준</p>
